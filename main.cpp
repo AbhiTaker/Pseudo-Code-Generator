7766af1e0f4556dd94786dc98e0210d18170c2c2
AbhiTaker <Abhi.taker20@gmail.com>
2018-02-16 20:23:52 +0530

    data structure added to detect the keyword

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+set<string> data_type, condition, loop;
+
+void intialize()
+{
+    /* store the appropriate keyword in appropriate set*/
+    data_type = {"int", "float", "string", "char", "double"};
+    condition = {"if"};
+    loop = {"for", "while"};
+}
 
 int check(string word)
 {
 
 }
+
 int main()
 {
     ifstream inFile("test.cpp");
+    intialize();
     string line;
     int i = 0;
 
